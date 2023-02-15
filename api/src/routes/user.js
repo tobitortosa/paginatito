1146a737991345e73c78a6a897f5af847dd77482
Tobi Tortosa <tobiastortosa17@gmail.com>
2023-02-15 16:01:03 -0300

    new commit

---
+++ api/src/routes/user.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const router = Router();
+const { User } = require("../db");
+const { passport } = require8("passport-local");
+
+router.post("/login", (req, res) => {});
+
+module.exports = router;
