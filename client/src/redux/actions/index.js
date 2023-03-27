b29edbbd1bcf7f5fdd79e2e9e1cdc79bafb2466e
Tobi Tortosa <tobiastortosa17@gmail.com>
2023-03-27 12:13:28 -0300

    change backend

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -24,7 +24,8 @@ import {
 } from "./actionsTypes";
 import axios from "axios";
 
-const url = "http://localhost:3001";
+// const url = "http://localhost:3001";
+const url = "https://paginatito.onrender.com";
 
 // Login
 
